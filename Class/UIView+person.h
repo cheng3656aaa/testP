5ee7a1d519bbcd07852d8acd4ddd7ce147aac748
chengyanlin <365616804@qq.com>
2017-03-28 15:59:55 +0800

    testP

---
+++ Class/UIView+person.h
@@ -0,0 +1,13 @@
+//
+//  UIView+person.h
+//  dawd awd
+//
+//  Created by CYL－Mac on 2017/3/27.
+//  Copyright © 2017年 CYL－Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (person)
+
+@end
---
+++ Class/UIView+person.m
@@ -0,0 +1,13 @@
+//
+//  UIView+person.m
+//  dawd awd
+//
+//  Created by CYL－Mac on 2017/3/27.
+//  Copyright © 2017年 CYL－Mac. All rights reserved.
+//
+
+#import "UIView+person.h"
+
+@implementation UIView (person)
+
+@end
